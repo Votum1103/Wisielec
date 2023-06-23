631578a0af8fb5d54bd7e8140db91e209697a4af
Julia Białas <20juliabialas02@gmail.com>
2023-06-23 11:22:55 +0200

    Dodano instrukcje

--- apka_1.py
+++ apka_1.py
@@ -172,6 +172,44 @@ class Ui_dialog_game(object):
         self.label_single_pict.setPixmap(QtGui.QPixmap("../../Downloads/sing.png"))
         self.label_single_pict.setScaledContents(True)
         self.label_single_pict.setObjectName("label_single_pict")
+        self.groupBox_instruction = QtWidgets.QGroupBox(dialog_game)
+        self.groupBox_instruction.setGeometry(QtCore.QRect(30, 170, 611, 381))
+        font = QtGui.QFont()
+        font.setFamily("Helvetica")
+        font.setPointSize(18)
+        self.groupBox_instruction.setFont(font)
+        self.groupBox_instruction.setStyleSheet("color: white;")
+        self.groupBox_instruction.setObjectName("groupBox_instruction")
+        self.textBrowser_instruction_text = QtWidgets.QTextBrowser(self.groupBox_instruction)
+        self.textBrowser_instruction_text.setGeometry(QtCore.QRect(20, 50, 581, 211))
+        self.textBrowser_instruction_text.setObjectName("textBrowser_instruction_text")
+        self.pushButton_instruction_wroc = QtWidgets.QPushButton(self.groupBox_instruction)
+        self.pushButton_instruction_wroc.setGeometry(QtCore.QRect(480, 310, 113, 32))
+        self.pushButton_instruction_wroc.setStyleSheet("QPushButton {\n"
+"    color: #565A5B;\n"
+"    border: 1px solid black;\n"
+"    font-size: 14px;\n"
+"    border-radius: 10px;\n"
+"    background-color: #C5DEEB;\n"
+"    padding: 3px;\n"
+"    }\n"
+"\n"
+"QPushButton:hover {\n"
+"    background: qradialgradient(\n"
+"        cx: 0.3, cy: -0.4, fx: 0.3, fy: -0.4,\n"
+"        radius: 1.35, stop: 0 #fff, stop: 1 #A5E2FF\n"
+"        );\n"
+"    }\n"
+"\n"
+"QPushButton:pressed {\n"
+"    color: #0B5AE1;\n"
+"    border-style: inset;\n"
+"    background: qradialgradient(\n"
+"        cx: 0.4, cy: -0.1, fx: 0.4, fy: -0.1,\n"
+"        radius: 1.35, stop: 0 #fff, stop: 1 #D7B5F9\n"
+"        );\n"
+"    }")
+        self.pushButton_instruction_wroc.setObjectName("pushButton_instruction_wroc")
 
         self.retranslateUi(dialog_game)
         QtCore.QMetaObject.connectSlotsByName(dialog_game)
@@ -183,6 +221,17 @@ class Ui_dialog_game(object):
         self.pushButton_multiplayer.setText(_translate("dialog_game", "MULTIPLAYER"))
         self.pushButton_instruction.setText(_translate("dialog_game", "INSTRUKCJA"))
         self.Wisielec_napis.setText(_translate("dialog_game", "Wisielec"))
+        self.groupBox_instruction.setTitle(_translate("dialog_game", "Instrukcja"))
+        self.textBrowser_instruction_text.setHtml(_translate("dialog_game", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'.AppleSystemUIFont\'; font-size:13pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt; color:#ffffff;\">1.  W grze multiplayer gracz pierwszy podaje słowo i oprzekazuje urządzenie graczowi drugiemu,  który zgaduje słowo wpisane przez gracza pierwszego. </span></p>\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt; color:#ffffff;\">2.  W grze singleplayer gracz zgaduje wymyślone przez komputer słowo. </span></p>\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt; color:#ffffff;\">3.  Niedopuszczalne jest definiowanie słów z myślnikami czy znakami białymi.   </span></p>\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt; color:#ffffff;\">4.  Słowa zgadujemy,  wpisując po jednej literze alfabetu polskiego. </span></p>\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt; color:#ffffff;\">5.  Jeśli domyślasz się,  jakie będzie słowo zanim zgadniesz wszystkie litery, możesz podjąć próbę odgadnięcia go,  wciskając przycisk &quot;tak&quot; w polu &quot;Czy chcesz odgadnąć hasło&quot;,  a następnie wpisując słowo.  W razie nieudanej próby zostanie odjęte 5 pkt. </span></p></body></html>"))
+        self.pushButton_instruction_wroc.setText(_translate("dialog_game", "Wróć"))
 
 
 if __name__ == "__main__":
